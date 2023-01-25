83d2831c26bf253de0ec7c3f59c15a60b09e91e5
stokeret <119980844+stokeret@users.noreply.github.com>
2023-01-25 15:03:24 +0000

    uploaded some initial files such as hello world.

---
+++ ForLoop/ForLoop.c
@@ -0,0 +1,14 @@
+// C for loop example
+// integer i is greater than or equal to 5 the program 
+// counts to 5 and displays it in the terminal#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    for (i; i <= 5; i++)
+    {
+        printf("%d\n", i);
+    }
+
+}
---
+++ HelloWorld/hello.c
@@ -0,0 +1,9 @@
+// Program uses the printf command to print 
+// "Hello World in the terminal"
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+}
